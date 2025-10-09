7312cba43f6aeb348f5fd0e45d0515e814b4ef96
ceoz <chitacode@gmail.com>
2025-10-09 14:19:54 -0300

    flowrena 12

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { CartContext } from '../context/CartContext'
 import Carrito from '/src/assets/cart3.png'
 
 
 function CartWidget() {
+  const { cart } = useContext(CartContext)
+
   return (
-    <div>
-      <img src={Carrito} alt="Carrito " style={{ width: '150px', height: 'auto' }} />
-      
+    <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+      <img src={Carrito} alt="Carrito" style={{ width: '50px', height: 'auto' }} />
+      <span>{cart.length}</span>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
+import { CartProvider } from './context/CartContext'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App/>
+   <CartProvider>
+        <App />
+   </CartProvider>
  </BrowserRouter>
 );
